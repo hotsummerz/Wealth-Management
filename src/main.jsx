ac1e192042307027bd898c6db8023a8004399d96
K <thegr8kii@gmail.com>
2026-06-22 21:57:05 +0700

    debug

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+console.log('ENV CHECK:', {
+  url: import.meta.env.VITE_SUPABASE_URL,
+  hasKey: !!import.meta.env.VITE_SUPABASE_ANON_KEY
+});
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
